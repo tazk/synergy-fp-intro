9233898b2e65eae0fc41daaddeccda0832000fd7
Nick Linker <nlinker@gmail.com>
2017-06-08 05:58:43 +0600

    test solutions

--- src/main/java/lesson1/Lesson01.java
+++ src/main/java/lesson1/Lesson01.java
@@ -1,6 +1,8 @@
 package lesson1;
 
+import java.util.Collections;
 import java.util.function.Function;
+import java.util.stream.Stream;
 import function.Function2;
 
 /**
@@ -12,14 +14,16 @@ public class Lesson01 {
      * TODO: Write a function that takes an input string and returns it uppercased.
      */
     public static final Function<String, String> upperCaser = str -> {
-        throw new NoSuchMethodError("upperCaser");
+        // throw new NoSuchMethodError("upperCaser");
+        return str.toUpperCase();
     };
 
     /**
      * TODO: Write a function that takes a string and number and returns concatenated string
      */
     public static final Function2<String, Integer, String> concatenator = (str, n) -> {
-        throw new NoSuchMethodError("concatenator");
+        // throw new NoSuchMethodError("concatenator");
+        return String.join("", Collections.nCopies(n, str));
     };
 
     /**
